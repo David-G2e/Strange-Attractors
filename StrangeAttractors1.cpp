b1bf332ed95e0b494b0dfca17a82e35de0c515df
David <112148921+David-G2e@users.noreply.github.com>
2024-09-24 11:33:13 -0700

    Update StrangeAttractors1.cpp

--- StrangeAttractors1.cpp
+++ StrangeAttractors1.cpp
@@ -1,23 +1,10 @@
 /***********************************************************************
-Animation - Example program demonstrating data exchange between a
-background animation thread and the foreground rendering thread using
-a triple buffer, and retained-mode OpenGL rendering using vertex and
-index buffers.
-Copyright (c) 2014-2021 Oliver Kreylos
+StrangeAttractors1 - Program modeling the Lorentz Attractor with 1000 particles.
+Data is exchanged between a background animation thread and the foreground 
+rendering thread using a triple buffer, and retained-mode OpenGL rendering
+using vertex and index buffers. 
 
-This program is free software; you can redistribute it and/or modify it
-under the terms of the GNU General Public License as published by the
-Free Software Foundation; either version 2 of the License, or (at your
-option) any later version.
-
-This program is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+Last Modified: 8/24/24
 ***********************************************************************/
 
 #include <unistd.h>
