cd2e1c558a594fbc45f7c4f1b27203b35b662809
David <112148921+David-G2e@users.noreply.github.com>
2024-09-24 11:31:43 -0700

    Update StrangeAttractors.cpp

--- StrangeAttractors.cpp
+++ StrangeAttractors.cpp
@@ -5,14 +5,7 @@ background animation thread and the foreground rendering thread using
 a triple buffer, and retained-mode OpenGL rendering using vertex and
 index buffers. A ring buffer is utilized to add particles. 
 
-This program is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+Last Modified: 9/24/24
 ***********************************************************************/
 
 #include <unistd.h>
